7e1dac660d60dc79cb48b9cacc34919df1572068
Muhammad Taha <mohamedengineer944@gmail.com>
2021-04-29 21:10:40 +0200

    Adding Modules to the project

---
+++ app.py
@@ -0,0 +1,5 @@
+################Importing modules################
+import json  # for database files
+import re  # for regex pattern
+import datetime  # for date and time
+################User Starting Menu################
