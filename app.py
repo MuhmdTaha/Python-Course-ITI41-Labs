f602b1e5692d23407e623a71454a13b5a09a38be
Muhammad Taha <mohamedengineer944@gmail.com>
2021-04-29 21:13:50 +0200

    Adding User Interface and regex

--- app.py
+++ app.py
@@ -3,3 +3,16 @@ import json  # for database files
 import re  # for regex pattern
 import datetime  # for date and time
 ################User Starting Menu################
+print("~~~Welcome to Crowdfunding App~~~\nChoose which process do you want?\n")
+print("1 --> Registeration\n2 --> Login\n3 --> Exit")
+
+select = input("Please Please Enter Your Choice: ")
+
+new_user = {}
+new_project = {}
+is_logged = False
+is_exist = False
+author = ''
+################Phone and Email Pattern################
+phone_regex = '^01[0125][0-9]{8}'
+email_regix = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
