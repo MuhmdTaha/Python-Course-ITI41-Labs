cbff3cddb27344b3fba60a328b7da0add53d9ed1
MuhmdTaha <72986780+MuhmdTaha@users.noreply.github.com>
2021-04-29 21:11:19 +0200

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-################Importing modules################
-import json  # for database files
-import re  # for regex pattern
-import datetime  # for date and time
-################User Starting Menu################
