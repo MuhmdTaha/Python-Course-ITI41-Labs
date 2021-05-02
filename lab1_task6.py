42e7d81db353dd1a4ad6b24db5244956bac2ffd5
Muhammad Taha <mohamedengineer944@gmail.com>
2021-05-02 05:42:11 +0200

    circle area and circumference

---
+++ lab1_task6.py
@@ -0,0 +1,24 @@
+
+############################### LAB-01 TASK-06 ##############################################
+
+# 6 - Ask the user to enter the radius of a circle in order to alert its calculated
+# area and circumference
+
+############################### START  TASK 6  ##############################################
+
+import math
+
+radius = float(input("Enter the radius of the circle: "))
+area = math.pi * radius * radius
+circumference = 2 * math.pi * radius
+
+print(f"Area of circle is: {area}, and its circumference is: {circumference}")
+
+#print(f"Area of circle is: {area}, and its circumference is: {circumference}")
+
+
+
+
+############################### END OF TASK 6  ##############################################
+
+
