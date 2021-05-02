1b9261f837198f56d3e7c65379a314e76966432a
Muhammad Taha <mohamedengineer944@gmail.com>
2021-05-02 05:41:32 +0200

    reverse a string

---
+++ lab1_task5.py
@@ -0,0 +1,13 @@
+
+############################### LAB-01 TASK-05 ##############################################
+
+# 5 - Write a function which has an input of a string from user then it will
+# return the same string reversed
+
+############################### START  TASK 5  ##############################################
+
+string_input = input("enter any string to be reversed: ")
+string_input = string_input.lower()
+print(string_input[::-1])
+
+############################### END OF TASK 5  ##############################################
